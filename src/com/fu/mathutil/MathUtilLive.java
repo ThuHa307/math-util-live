e3ef2ba8fb77899e308f3387b2b56ef6fb57df12
ThuHa307 <hatran3072003@gmail.com>
2023-05-22 21:34:05 +0700

    Re-test

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -13,6 +13,9 @@ public class MathUtilLive {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = " + result);
+        
+        //Thêm code sau khi push lên server 9:31PM 22/5/2023
+        System.out.println("6! = " + MathUtil.getFactorial(6));
         //MathUtil.getFactorial(-5);
     }
     
